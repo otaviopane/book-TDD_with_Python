6c535a5bfe1e0987016e46ddb741c3228e72b2e3
Otavio F. G. Pane <otavio.pane@gmail.com>
2021-10-06 18:57:29 -0300

    DONE - 2º Cap of TDD with Python - First FT specced out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -8,8 +8,49 @@ servida pelo microcomputador local;
 “Django” no título.'''
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Jorge ouviu falar de uma nova aplicação online interessante para
+        # lista de tarefas. Ele decide verificar sua homepage
+        self.browser.get('http://localhost:8000')
+
+        # Ele percebe que o título da página e o cabeçalho mencionam
+        # listas de tarefas (to-do)
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+    # Ele é convidado a inserir um item de tarefa imediatamente
+
+    # Ele digita "Buy peacock feathers" (Comprar penas de pavão) em uma caixa
+    # de texto (o hobby de Jorge é fazer iscas para pesca com fly)
+
+    # Quando ela tecla enter, a página é atualizada, e agora a página lista
+    # "1: Buy peacock feathers" como um item em uma lista de tarefas
+
+    # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item.
+    # Ele insere "Use peacock feathers to make a fly"(Usar penas de pavão
+    # para fazer uum fly - Jorge é bem metódico)
+
+    # A página é atualizada novamente e agora mostra os dois itens em sua lista
+
+    # Jorge se pergunta se o site lembrará de sua lista.
+    # Então ele nota que o site gerou um URL único para ele
+    # -- há um pequeno texto explicativo para isso.
+
+    # Ele acessa esse URL - sua lista de tarefas continua lá
+
+    # Satisfeito, ele volta a dormir
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
